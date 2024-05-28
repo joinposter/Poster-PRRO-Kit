59669d2d7f3a83305e37bd7f089d3ebc7a092e7b
m.babarov <m.babarov@joinposter.com>
2024-05-28 19:44:22 +0300

    add generateXReport into public methods

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import generateTextFiscalReceipt from "./lib/generateTextFiscalReceipt/index.js";
 import generateTextServiceInputReceipt from "./lib/generateTextServiceInputReceipt/index.js";
 import generateTextServiceOutputReceipt from "./lib/generateTextServiceOutputReceipt/index.js";
+import generateXReport from "./lib/generateXReport/index.js";
 import generateQRCodeReceiptData from "./lib/generateQRCodeReceiptData.js";
 
 export {
   generateTextFiscalReceipt,
   generateTextServiceInputReceipt,
   generateTextServiceOutputReceipt,
+  generateXReport,
   generateQRCodeReceiptData,
 };

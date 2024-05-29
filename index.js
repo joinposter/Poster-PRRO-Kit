b722df526737f599f7d036c06a7cb9b8fe30ef9c
m.babarov <m.babarov@joinposter.com>
2024-05-29 17:23:32 +0300

    refactoring, update test, fix lint issue

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import generateTextFiscalReceipt from "./lib/generateTextFiscalReceipt/index.js";
 import generateTextServiceTransactionReceipt from "./lib/generateTextServiceTransactionReceipt/index.js";
-import generateXReport from "./lib/generateXReport/index.js";
+import generateXZReport from "./lib/generateXZReport/index.js";
 import generateQRCodeReceiptData from "./lib/generateQRCodeReceiptData.js";
 
 export {
   generateTextFiscalReceipt,
   generateTextServiceTransactionReceipt,
-  generateXReport,
+  generateXZReport,
   generateQRCodeReceiptData,
 };

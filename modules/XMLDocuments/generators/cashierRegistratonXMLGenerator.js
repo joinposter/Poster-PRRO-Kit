c70bdd869e220ed508c6d6a5732c4e4298f5db4a
m.babarov <m.babarov@joinposter.com>
2025-09-02 11:16:30 +0300

    refactoring after review

--- modules/XMLDocuments/generators/cashierRegistratonXMLGenerator.js
+++ modules/XMLDocuments/generators/cashierRegistratonXMLGenerator.js
@@ -1,20 +1,31 @@
 import { getRowNum } from "./commonXMLTagGenerator.js";
 
+// eslint-disable-next-line no-magic-numbers
+const getRegionCode = (dpiCode) => dpiCode?.toString().slice(0, -2);
+
+// eslint-disable-next-line no-magic-numbers
+const getDistrictCode = (dpiCode) => dpiCode?.toString().slice(-2);
+
+const getDocumentCode = (isLLC) => (isLLC ? "J13" : "F13");
+
+const getCurrentDate = () => {
+  const date = new Date();
+  // eslint-disable-next-line no-magic-numbers
+  const day = String(date.getDate()).padStart(2, "0");
+  // eslint-disable-next-line no-magic-numbers
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const year = date.getFullYear();
+  return `${day}${month}${year}`;
+};
+
 const getCashierRegistrationDocument = (data) => {
   const {
-    cDoc,
+    isLLC,
     documentNumber,
-    regionCode,
-    districtCode,
+    taxCodeDPI,
     reportingMonth,
     reportingYear,
-    taxCodeDPI,
-    fillDate,
-    softwareName,
-
     // Тіло декларації
-    operationDate,
-    operationNumber,
     organizationTin,
     organizationName,
 
@@ -32,25 +43,25 @@ const getCashierRegistrationDocument = (data) => {
   const DECLAR = {
     DECLARHEAD: {
       TIN: organizationTin,
-      C_DOC: cDoc,
+      C_DOC: getDocumentCode(isLLC),
       C_DOC_SUB: "918",
       C_DOC_VER: 2,
       C_DOC_TYPE: 0,
       C_DOC_CNT: documentNumber,
-      C_REG: regionCode,
-      C_RAJ: districtCode,
+      C_REG: getRegionCode(taxCodeDPI),
+      C_RAJ: getDistrictCode(taxCodeDPI),
       PERIOD_MONTH: reportingMonth,
       PERIOD_TYPE: 1,
       PERIOD_YEAR: reportingYear,
       C_STI_ORIG: taxCodeDPI,
       C_DOC_STAN: 1,
       LINKED_DOCS: { $: { "xsi:nil": "true" } },
-      D_FILL: fillDate,
-      SOFTWARE: softwareName,
+      D_FILL: getCurrentDate(),
+      SOFTWARE: "Poster",
     },
     DECLARBODY: {
-      HDATE: operationDate,
-      HNUM: operationNumber,
+      HDATE: getCurrentDate(),
+      HNUM: documentNumber,
       HTIN: organizationTin,
       HNAME: organizationName,
       T1RXXXXG1S: [{ _: cashierFullName, $: getRowNum(0) }],
@@ -60,7 +71,7 @@ const getCashierRegistrationDocument = (data) => {
       M01: 1,
       HKBOS: chiefTin,
       HBOS: chiefName,
-      HFILL: fillDate,
+      HFILL: getCurrentDate(),
     },
   };
 
@@ -68,7 +79,7 @@ const getCashierRegistrationDocument = (data) => {
     DECLAR: {
       $: {
         "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
-        "xsi:noNamespaceSchemaLocation": "J1391802.xsd",
+        "xsi:noNamespaceSchemaLocation": `${isLLC ? "J1391802.xsd" : "F1391802.xsd"}`,
       },
       ...DECLAR,
     },

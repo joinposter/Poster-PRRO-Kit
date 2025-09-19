01ac277faf4392cbc1fe3fb08d90a8b578b49e64
vitfomenko <vit.fomenco@gmail.com>
2025-09-19 15:51:44 +0300

    fix crash receipt print on table data

--- modules/receiptGenerator/formatters/text/smartTable/smartTableFormatter.js
+++ modules/receiptGenerator/formatters/text/smartTable/smartTableFormatter.js
@@ -2,12 +2,36 @@
 import narrowTableFormatter from "./narrowTableFormatter.js";
 import wideTableFormatter from "./wideTableFormatter.js";
 
+const RE_ASCII_CONTROL_CHARS = /[\x00-\x1F]+/g;
+const RE_UNICODE_CONTROL_CHARS = /[\u0001-\u001A]+/g;
+
+const cleanControlChars = (value) => {
+  if (typeof value === "string") {
+    return value
+      .replace(RE_ASCII_CONTROL_CHARS, " ")
+      .replace(RE_UNICODE_CONTROL_CHARS, " ")
+  }
+
+  return value;
+};
+
+const cleanTableParams = (params) => {
+  return {
+    ...params,
+    items: params.items.map(item => ({
+      ...item,
+      row: item.row.map(cleanControlChars)
+    }))
+  };
+};
+
 function smartTableFormatter(params, config) {
+  const cleanedParams = cleanTableParams(params);
   const { formatters } = this;
   const { width } = config;
   return width < 38
-    ? narrowTableFormatter(params, config, formatters)
-    : wideTableFormatter(params, config, formatters);
+    ? narrowTableFormatter(cleanedParams, config, formatters)
+    : wideTableFormatter(cleanedParams, config, formatters);
 }
 
 export default smartTableFormatter;
